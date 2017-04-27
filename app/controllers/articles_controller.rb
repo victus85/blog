d37b2288f22dad657997a52a0a2dda8c9e7f04b3
Victor <vmartinez@synergie.es>
2017-04-27 17:56:55 +0200

    commit_270417
    adding validations saving articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,28 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
+
+
+    if @article.save
     redirect_to @article
+  else
+      render 'new'
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+#  def show
+#    @article = Article.find(params[:id])
+#  end
 
-  def index
-    @articles = Article.all
-  end
+#  def index
+#    @articles = Article.all
+#  end
 
   private
     def article_params
     params.require(:article).permit(:title, :text)
     end
-
-
-
 end
