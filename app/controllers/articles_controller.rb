5268c8a7f2996a2fef4113c96adc957294fe1732
Victor <vmartinez@synergie.es>
2017-04-27 14:01:10 +0200

    commit_20170427
    visualizar artículos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redirect_to @article
   end
@@ -11,7 +11,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  def index
+    @articles = Article.all
+  end
+
   private
     def article_params
     params.require(:article).permit(:title, :text)
