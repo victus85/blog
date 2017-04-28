dd6092fe29db2abf6e43f3ab2fc705c4f38aa767
Victor <vmartinez@synergie.es>
2017-04-28 14:43:12 +0200

    funcionando
    links_ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,16 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-
     if @article.save
     redirect_to @article
   else
       render 'new'
   end
+end
+
+def index
+  @articles = Article.all
+end
 
 #  def show
 #    @article = Article.find(params[:id])
@@ -26,5 +30,4 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :text)
     end
 
-end
 end
